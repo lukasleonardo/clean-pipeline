71461539086fca1be298e5f99a704bcefbec20b7
Lucas Henrique <lucas.hcleonardo@gmail.com>
2023-11-04 12:36:26 -0300

    estagio inicial do genre service

--- src/genre/entities/genre.entity.ts
+++ src/genre/entities/genre.entity.ts
@@ -1,12 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 import { IGenre } from '../interfaces/genre.interface';
 import { v4 as uuidV4 } from 'uuid';
 
 @Entity('genre')
 export class GenreEntity implements IGenre {
-  @PrimaryGeneratedColumn()
-  id: number;
-  @Column({ length: 50 })
+  @PrimaryColumn()
+  id: string;
+  @Column({ length: 300 })
   name: string;
 
   constructor() {
--- src/genre/interfaces/genre.interface.ts
+++ src/genre/interfaces/genre.interface.ts
@@ -1,4 +1,4 @@
 export interface IGenre {
-  id?: number;
+  id?: string;
   name: string;
 }
