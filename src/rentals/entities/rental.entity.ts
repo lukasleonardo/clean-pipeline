4204a3bd0eb766b4d116b99db7c95aba905bb0cf
Lucas Henrique <lucas.hcleonardo@gmail.com>
2023-11-12 11:16:06 -0300

    correção de entidades

--- src/rentals/entities/rental.entity.ts
+++ src/rentals/entities/rental.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { UserEntity } from "../../user/entities/user.entity";
 import { BookEntity } from "../../book/entities/book.entity";
 
@@ -19,9 +19,8 @@ export class RentalEntity {
   @JoinColumn()
   user: UserEntity;
 
-  @OneToOne(() => BookEntity, {eager:true})
+  @ManyToOne(() => BookEntity, {eager:true})
   @JoinColumn()
-  book: BookEntity;
-
+  book: BookEntity[];
 
 }
